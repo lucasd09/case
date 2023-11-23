b0fa97005fdeb01e827761e630be2e5ce2e63537
Lucas Dalan <75147965+lucasd09@users.noreply.github.com>
2023-11-23 18:03:31 -0300

    (feat) - homepage body

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,32 @@
-import { Button } from "@/components/ui/button";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import Navbar from "@/components/ui/navbar";
 import { Separator } from "@/components/ui/separator";
-import { ThemeSwitch } from "@/components/client-components/theme-switch";
 
 export default function Home() {
   return (
     <div>
-      <div className="h-16 border-b-2 flex items-center justify-around">
-        <ul className="flex space-x-4">
-          <li>
-            <a href="" className="">
-              Home
-            </a>
-          </li>
-          <li>
-            <Separator orientation="vertical" />
-          </li>
-          <li>
-            <a href="#" className="">
-              Como funciona
-            </a>
-          </li>
-        </ul>
-        <div className="flex h-5 items-center space-x-4 text-sm">
-          <ThemeSwitch />
-          <Separator orientation="vertical" />
-          <Button>Login</Button>
+      <Navbar />
+      <div className="h-screen flex items-center justify-around">
+        <div className="bg-secondary w-96 rounded-xl">
+          <div className="h-32 flex items-center">
+            <Avatar className="m-8">
+              <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+            </Avatar>
+            <div>
+              <h1 className=" ">Lucas Dalan</h1>
+              <p>Software Developer</p>
+            </div>
+          </div>
+          <div className="h-[256px] text-center">
+            <Separator />
+            <h1>Redes Sociais</h1>
+          </div>
         </div>
       </div>
-      <div>body</div>
-      <div>footer</div>
+      <div>
+        <Separator />
+        footer
+      </div>
     </div>
   );
 }
--- components/client-components/theme-switch.tsx
+++ components/client-components/theme-switch.tsx
@@ -4,11 +4,17 @@ import { Switch } from "../ui/switch";
 import { useTheme } from "next-themes";
 
 export function ThemeSwitch() {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
 
   return (
     <div className="flex items-center space-x-2">
-      <Switch id="theme-switch" onCheckedChange={() => setTheme("light")} />
+      <Switch
+        id="theme-switch"
+        checked={theme == "dark"}
+        onCheckedChange={() => {
+          theme == "light" ? setTheme("dark") : setTheme("light");
+        }}
+      />
       <Label htmlFor="theme-switch">Tema Escuro</Label>
     </div>
   );
