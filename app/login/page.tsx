1a3c8495215a85224c652339b5d312d5f622da3f
Lucas Dalan <75147965+lucasd09@users.noreply.github.com>
2023-11-28 11:38:27 -0300

    login e register

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,14 +4,20 @@ import { Separator } from "@/components/ui/separator";
 
 export default function Login() {
   return (
-    <div className="flex">
-      <div className="bg-secondary h-screen w-full">a</div>
-      <div className="flex flex-col justify-center w-[660px]">
-        <h1>Faça login em sua conta</h1>
-        <Input placeholder="Email" />
-        <Input placeholder="Senha" />
-        <Button>Login</Button>
-        <Separator />
+    <div className="flex h-screen bg-secondary items-center justify-end">
+      <div className="flex flex-col bg-card justify-center w-[385px] h-[600px] p-4 m-16 rounded-lg text-center border">
+        <h1 className="mb-8 text-lg">Faça login em sua conta</h1>
+        <Input placeholder="Email" className="mb-4" />
+        <Input placeholder="Senha" className="mb-4" />
+        <Button className="mb-4">Login</Button>
+        <div className="flex items-center justify-center mb-8">
+          <Separator className="w-32 mr-2" />
+          <p>OU</p>
+          <Separator className="w-32 ml-2" />
+        </div>
+        <Button className="mb-4" variant={"outline"}>
+          Google
+        </Button>
       </div>
     </div>
   );
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,3 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+
 export default function Register() {
-  return <div>register</div>;
+  return (
+    <div className="flex h-screen bg-secondary items-center justify-end">
+      <div className="flex flex-col bg-card justify-center w-[385px] h-[600px] p-4 m-16 rounded-lg text-center border">
+        <h1 className="text-lg">Cadastre-se gratuitamente</h1>
+        <h3 className="mb-8 text-sm">
+          Insira um email para continuar seu cadastro
+        </h3>
+        <Input placeholder="Email" className="mb-4" />
+        <Button className="mb-4">Cadastre-se</Button>
+        <div className="flex items-center justify-center mb-8">
+          <Separator className="w-24 mr-2" />
+          <p>Ou continue com</p>
+          <Separator className="w-24 ml-2" />
+        </div>
+        <Button className="mb-4" variant={"outline"}>
+          Google
+        </Button>
+      </div>
+    </div>
+  );
 }
